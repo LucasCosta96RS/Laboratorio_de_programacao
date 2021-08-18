953353ea73e82378d545c088407742c7bcec6ad3
Lucas Costa <lucascosta96rs@gmail.com>
2021-08-18 00:12:09 -0300

    Feito o exercício 12 do TDE01

---
+++ TDE01/Ex12.java
@@ -0,0 +1,16 @@
+package TDE01;
+
+import java.util.Scanner;
+
+public class Ex12 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double peso;
+        double pesoLua;
+        System.out.println("Digite o seu peso:");
+        peso = teclado.nextFloat();
+        pesoLua = peso * 0.17;
+        System.out.println("O seu peso na lua é: " + pesoLua);
+    }
+}
---
+++ out/production/Laboratorio_de_programacao/README.md
@@ -0,0 +1,2 @@
+# Laboratorio_de_programacao
+ Repositório para colocar os exercícios feitos das aulas de laboratório de programação
