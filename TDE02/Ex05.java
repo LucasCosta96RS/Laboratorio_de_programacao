54b071fb7dc6600c6f7d8567127855f56bdc01dc
Lucas Costa <lucascosta96rs@gmail.com>
2021-08-18 17:21:39 -0300

    Feito o exercício 05 do TDE02

---
+++ TDE02/Ex05.java
@@ -0,0 +1,35 @@
+package TDE02;
+
+import java.util.Scanner;
+
+public class Ex05 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double valorProduto;
+        double valorComDesconto;
+        String nomeProduto;
+        System.out.println("Digite o nome do produto: ");
+        nomeProduto = teclado.next();
+        System.out.println("Digite o valor do produto: ");
+        valorProduto = teclado.nextDouble();
+        if (valorProduto < 50 && valorProduto > 0) {
+            System.out.println("O produto " + nomeProduto + " de valor " + valorProduto + " não se aplica a nenhum desconto.");
+        } else if (valorProduto >= 50 && valorProduto < 200) {
+            valorComDesconto = valorProduto - (valorProduto * 0.05);
+            System.out.println("O produto " + nomeProduto + " de valor " + valorProduto + " com 5% de desconto ficará " + valorComDesconto);
+        }else if (valorProduto >= 200 && valorProduto < 500) {
+            valorComDesconto = valorProduto - (valorProduto * 0.06);
+            System.out.println("O produto " + nomeProduto + " de valor " + valorProduto + " com 6% de desconto ficará " + valorComDesconto);
+        } else if (valorProduto >= 500 && valorProduto < 1000) {
+            valorComDesconto = valorProduto - (valorProduto * 0.07);
+            System.out.println("O produto " + nomeProduto + " de valor " + valorProduto + " com 7% de desconto ficará " + valorComDesconto);
+        } else if (valorProduto >= 1000) {
+            valorComDesconto = valorProduto - (valorProduto * 0.08);
+            System.out.println("O produto " + nomeProduto + " de valor " + valorProduto + " com 8% de desconto ficará " + valorComDesconto);
+        } else {
+            System.out.println("O valor digitado não é aceito pelo sistema.");
+        }
+    }
+}
+
