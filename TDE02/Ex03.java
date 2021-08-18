2a347a9e82dc9509bd32b91c48d4bb98b71fb13f
Lucas Costa <lucascosta96rs@gmail.com>
2021-08-18 16:03:02 -0300

    Feito o exercício 03 do TDE02

---
+++ TDE02/Ex03.java
@@ -0,0 +1,29 @@
+package TDE02;
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+public class Ex03 {
+
+    private static final DecimalFormat dft = new DecimalFormat("#.##");
+
+    public static void main(String[] args) {
+        double notaProva1;
+        double notaProva2;
+        double notaTrabalho;
+        double media;
+        String digitado;
+        digitado = JOptionPane.showInputDialog("Digite a primeira nota");
+        notaProva1 = Double.parseDouble(digitado);
+        digitado = JOptionPane.showInputDialog("Digite a segunda nota");
+        notaProva2 = Double.parseDouble(digitado);
+        digitado = JOptionPane.showInputDialog("Digite a nota do trabalho");
+        notaTrabalho = Double.parseDouble(digitado);
+        media = (notaProva1 + notaProva2 + notaTrabalho) / 3;
+        if (media >= 6) {
+            JOptionPane.showMessageDialog(null, "Você tirou nota " + dft.format(media) + " e foi aprovado!");
+        }else {
+            JOptionPane.showMessageDialog(null, "Você tirou nota " + dft.format(media) + " e foi reprovado!");
+        }
+    }
+}
