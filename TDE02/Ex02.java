7abbce8e0d556fea0d36c89709fbdc197e0bfd34
Lucas Costa <lucascosta96rs@gmail.com>
2021-08-18 01:23:54 -0300

    Feito o exercício 02 do TDE02

---
+++ TDE02/Ex02.java
@@ -0,0 +1,27 @@
+package TDE02;
+
+import javax.swing.*;
+
+public class Ex02 {
+
+    public static void main(String[] args) {
+        double valorTransacao;
+        double valorVenal;
+        double percentualDesconto;
+        double valorImposto;
+        String digitado;
+        digitado = JOptionPane.showInputDialog("Digite o valor da transação: ");
+        valorTransacao = Double.parseDouble(digitado);
+        digitado = JOptionPane.showInputDialog("Digite o valor venal de refêrencia: ");
+        valorVenal = Double.parseDouble(digitado);
+        digitado = JOptionPane.showInputDialog("Digite o percentual de Desconto: ");
+        percentualDesconto = Double.parseDouble(digitado);
+        if (valorTransacao > valorVenal) {
+            valorImposto = valorTransacao * percentualDesconto / 100;
+        }
+        else {
+            valorImposto = valorVenal * percentualDesconto / 100;
+        }
+        JOptionPane.showMessageDialog(null, "O valor do imposto será de: " + valorImposto);
+    }
+}
