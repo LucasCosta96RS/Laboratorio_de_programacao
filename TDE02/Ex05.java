852a40d10fd82e112da9ecd6c881da0351645f68
Lucas Costa <lucascosta96rs@gmail.com>
2021-08-20 00:04:48 -0300

    Feito pequenos ajustes

--- TDE02/Ex05.java
+++ TDE02/Ex05.java
@@ -11,22 +11,22 @@ public class Ex05 {
         String nomeProduto;
         System.out.println("Digite o nome do produto: ");
         nomeProduto = teclado.next();
-        System.out.println("Digite o valor do produto: ");
+        System.out.println("Digite o valor do produto em R$: ");
         valorProduto = teclado.nextDouble();
         if (valorProduto < 50 && valorProduto > 0) {
-            System.out.println("O produto " + nomeProduto + " de valor " + valorProduto + " não se aplica a nenhum desconto.");
+            System.out.println("O produto " + nomeProduto.toUpperCase() + " de valor R$" + valorProduto + " não se aplica a nenhum desconto.");
         } else if (valorProduto >= 50 && valorProduto < 200) {
             valorComDesconto = valorProduto - (valorProduto * 0.05);
-            System.out.println("O produto " + nomeProduto + " de valor " + valorProduto + " com 5% de desconto ficará " + valorComDesconto);
+            System.out.println("O produto " + nomeProduto.toUpperCase() + " de valor R$" + valorProduto + " com 5% de desconto ficará R$" + valorComDesconto);
         }else if (valorProduto >= 200 && valorProduto < 500) {
             valorComDesconto = valorProduto - (valorProduto * 0.06);
-            System.out.println("O produto " + nomeProduto + " de valor " + valorProduto + " com 6% de desconto ficará " + valorComDesconto);
+            System.out.println("O produto " + nomeProduto.toUpperCase() + " de valor R$" + valorProduto + " com 6% de desconto ficará R$" + valorComDesconto);
         } else if (valorProduto >= 500 && valorProduto < 1000) {
             valorComDesconto = valorProduto - (valorProduto * 0.07);
-            System.out.println("O produto " + nomeProduto + " de valor " + valorProduto + " com 7% de desconto ficará " + valorComDesconto);
+            System.out.println("O produto " + nomeProduto.toUpperCase() + " de valor R$" + valorProduto + " com 7% de desconto ficará R$" + valorComDesconto);
         } else if (valorProduto >= 1000) {
             valorComDesconto = valorProduto - (valorProduto * 0.08);
-            System.out.println("O produto " + nomeProduto + " de valor " + valorProduto + " com 8% de desconto ficará " + valorComDesconto);
+            System.out.println("O produto " + nomeProduto.toUpperCase() + " de valor R$" + valorProduto + " com 8% de desconto ficará R$" + valorComDesconto);
         } else {
             System.out.println("O valor digitado não é aceito pelo sistema.");
         }
