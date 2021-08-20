ebf639d31b104bb69373478444ed7e510d41851f
Lucas Costa <lucascosta96rs@gmail.com>
2021-08-20 00:06:42 -0300

    Trocado int por double e colocado arredondamento

--- TDE02/Ex08.java
+++ TDE02/Ex08.java
@@ -1,23 +1,26 @@
 package TDE02;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Ex08 {
 
+    private static final DecimalFormat dft = new DecimalFormat("#.##");
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int numero;
+        double numero;
         System.out.println("Digite o número que deseja ver a tabuada: ");
-        numero = teclado.nextInt();
-        System.out.println(numero + " X 1 = " + numero);
-        System.out.println(numero + " X 2 = " + numero * 2);
-        System.out.println(numero + " X 3 = " + numero * 3);
-        System.out.println(numero + " X 4 = " + numero * 4);
-        System.out.println(numero + " X 5 = " + numero * 5);
-        System.out.println(numero + " X 6 = " + numero * 6);
-        System.out.println(numero + " X 7 = " + numero * 7);
-        System.out.println(numero + " X 8 = " + numero * 8);
-        System.out.println(numero + " X 9 = " + numero * 9);
-        System.out.println(numero + " X 10 = " + numero * 10);
+        numero = teclado.nextDouble();
+        System.out.println(dft.format(numero) + " X 1 = " + dft.format(numero));
+        System.out.println(dft.format(numero) + " X 2 = " + dft.format(numero * 2));
+        System.out.println(dft.format(numero) + " X 3 = " + dft.format(numero * 3));
+        System.out.println(dft.format(numero) + " X 4 = " + dft.format(numero * 4));
+        System.out.println(dft.format(numero) + " X 5 = " + dft.format(numero * 5));
+        System.out.println(dft.format(numero) + " X 6 = " + dft.format(numero * 6));
+        System.out.println(dft.format(numero) + " X 7 = " + dft.format(numero * 7));
+        System.out.println(dft.format(numero) + " X 8 = " + dft.format(numero * 8));
+        System.out.println(dft.format(numero) + " X 9 = " + dft.format(numero * 9));
+        System.out.println(dft.format(numero) + " X 10 = " + dft.format(numero * 10));
     }
 }
