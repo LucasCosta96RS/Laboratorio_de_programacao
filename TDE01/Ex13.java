5b433dbfbd29fceb3500901b3d4e8bfe15b82eab
Lucas Costa <lucascosta96rs@gmail.com>
2021-08-19 23:57:13 -0300

    Separado a variável distancia

--- TDE01/Ex13.java
+++ TDE01/Ex13.java
@@ -3,7 +3,8 @@ package TDE01;
 public class Ex13 {
 
     public static void main(String[] args) {
-        int distancia = (int) (1100 * 7.2);
+        int distancia;
+        distancia = (int) (1100 * 7.2);
         System.out.println("Se um observador vê um relâmpago e depois de 7,2 segundos ouvi o som do mesmo, ele está a " + distancia + "m de distância do próprio relâmpago.");
     }
 }
