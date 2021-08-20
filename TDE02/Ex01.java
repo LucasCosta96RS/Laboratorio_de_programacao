ba0f3804191157b59ed29309189dee5407f17359
Lucas Costa <lucascosta96rs@gmail.com>
2021-08-20 00:00:44 -0300

    Separado as variáveis

--- TDE02/Ex01.java
+++ TDE02/Ex01.java
@@ -6,11 +6,14 @@ public class Ex01 {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        int valorDesconto;
+        double valorProduto;
+        double valorComDesconto;
         System.out.println("Qual o valor do produto em R$?");
-        float valorProduto = teclado.nextFloat();
+        valorProduto = teclado.nextDouble();
         System.out.println("Quanto é o desconto a ser aplicado?");
-        int valorDesconto = teclado.nextInt();
-        float valorComDesconto = valorProduto - (valorProduto * valorDesconto / 100);
+        valorDesconto = teclado.nextInt();
+        valorComDesconto = valorProduto - (valorProduto * valorDesconto / 100);
         System.out.println("O valor do produto com desconto de " + valorDesconto + "%" + " é R$" + valorComDesconto);
     }
 }
